d8999997076fdf86de5f116b2af7400a6e3408c2
DavitGe <davitge303@gmail.com>
2022-11-17 13:49:30 +0400

    @koa/cors

--- api/index.js
+++ api/index.js
@@ -1,13 +1,18 @@
 const Koa = require("koa");
+const Router = require("koa-router");
+
 const app = new Koa();
+const router = Router();
 
 const data = require("./data.json");
-const cors = require("cors");
+const cors = require("@koa/cors");
 
-app.use(async (ctx) => {
+app.use(cors());
+
+router.get("/", async (ctx, next) => {
   ctx.body = data;
 });
 
-app.use(cors());
+app.use(router.routes());
 
 app.listen(3001);
