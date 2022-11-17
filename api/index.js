155ef87c7366c7739e8edd69e3e81d0d148e4147
DavitGe <davitge303@gmail.com>
2022-11-17 12:53:54 +0400

    cors

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ const app = new Koa();
 const data = require("./data.json");
 const cors = require("cors");
 
-app.use(cors());
-
 app.use(async (ctx) => {
   ctx.body = data;
 });
+
+app.use(cors());
 
 app.listen(3001);
