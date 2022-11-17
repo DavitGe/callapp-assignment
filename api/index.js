89cc4b0a92c7bf3210f8449a09b484885247ef39
DavitGe <davitge303@gmail.com>
2022-11-17 12:52:57 +0400

    cors

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const Koa = require("koa");
 const app = new Koa();
 
 const data = require("./data.json");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(async (ctx) => {
   ctx.body = data;
