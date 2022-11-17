3962ba21377104a49ac92a606065b0ae8d8e8540
DavitGe <davitge303@gmail.com>
2022-11-17 13:50:00 +0400

    Fetching data

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 function App() {
+  fetch("http://localhost:3001/")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
   return (
     <div className="App">
       <p>Hello World</p>
