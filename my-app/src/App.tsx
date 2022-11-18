214f6ba6091a01b4c1f4549bc4c9c5f09f6d167e
DavitGe <davitge303@gmail.com>
2022-11-18 20:49:23 +0400

    Deployment

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,15 +8,21 @@ import Table from "./components/Table";
 import Header from "./components/Header";
 import CitiesChart from "./components/CitiesChart";
 
+import "dotenv/config";
+
 function App() {
   const { setUsers } = useUserStore((state) => ({
     setUsers: state.setUsers,
   }));
 
   useEffect(() => {
-    fetch("http://localhost:3001/")
-      .then((response) => response.json())
-      .then((data: User[]) => setUsers(data));
+    try {
+      fetch(process.env.URL ? process.env.URL : "")
+        .then((response) => response.json())
+        .then((data: User[]) => setUsers(data));
+    } catch (error) {
+      console.log("error", error);
+    }
     //eslint-disable-next-line
   }, []);
 
